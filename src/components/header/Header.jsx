739916fad445f070bf5ad67f27fda8f55230e607
NelsonMungai <nelson.irungu@students.jkuat.ac.ke>
2025-02-06 18:35:21 +0300

    Added the necessary assets, made the navbar for large screens

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,52 @@
+import "./header.css";
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav_logo">
+          Smith
+        </a>
+        <div className="nav_menu">
+          <ul className="nav_list grid">
+            <li className="list_item">
+              <a href="#home" className="nav_link active_link">
+                <i className="uil uil-estate nav_icon"></i> HOME
+              </a>
+            </li>
+            <li className="list_item">
+              <a href="#about" className="nav_link">
+                <i className="uil uil-user nav_icon"></i> ABOUT
+              </a>
+            </li>
+            <li className="list_item">
+              <a href="#skills" className="nav_link">
+                <i className="uil uil-file-alt nav_icon"></i> SKILLS
+              </a>
+            </li>
+            <li className="list_item">
+              <a href="#services" className="nav_link">
+                <i className="uil uil-briefcase-alt nav_icon"></i> SERVICES
+              </a>
+            </li>
+            <li className="list_item">
+              <a href="#portfolio" className="nav_link">
+                <i className="uil uil-scenery nav_icon"></i> PORFOLIO
+              </a>
+            </li>
+            <li className="list_item">
+              <a href="#contact" className="nav_link">
+                <i className="uil uil-message nav_icon"></i> CONTACTS
+              </a>
+            </li>
+          </ul>
+          <i className="uil uil-times nav_close"></i>
+        </div>
+        <div className="nav_toggle">
+          <i className="uil uil-apps nav_toggle"></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
