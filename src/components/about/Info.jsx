24c0c731c0e43a2a62c918c3b6f4065757fd1f3a
NelsonMungai <nelson.irungu@students.jkuat.ac.ke>
2025-02-13 13:32:31 +0300

    About Page done and responsive

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,22 @@
+const Info = () => {
+  return (
+    <div className="about_info grid">
+      <div className="about_box">
+        <i className="bx bx-award about-icon"></i>
+        <h3 className="about_title">Experience</h3>
+        <span className="about_subtitle">3 years Practising</span>
+      </div>
+      <div className="about_box">
+        <i className="bx bx-briefcase-alt about-icon"></i>
+        <h3 className="about_title">Completed</h3>
+        <span className="about_subtitle">48 + Projects</span>
+      </div>
+      <div className="about_box">
+        <i className="bx bx-support about-icon"></i>
+        <h3 className="about_title">Support</h3>
+        <span className="about_subtitle">24/7 Online</span>
+      </div>
+    </div>
+  );
+};
+export default Info;
